9a293ca794c7d7314ad7473f3a3465247b766738
denys-pavskyi <den.pavski@gmail.com>
2025-05-08 01:03:49 +0300

    feat: Added entities to DAL (User, Announcement, Category, Subcategory)

---
+++ BulletinBoard.DAL/Entities/Announcement.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulletinBoard.DAL.Entities;
+
+public class Announcement
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(150)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(1000)]
+    public string Description { get; set; } = string.Empty;
+
+    [Required]
+    public bool IsActive { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+    public int SubcategoryId { get; set; }
+
+    [ForeignKey(nameof(SubcategoryId))]
+    public Subcategory SubCategory { get; set; } = null!;
+
+    public int UserId { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; } = null!;
+
+}
---
+++ BulletinBoard.DAL/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulletinBoard.DAL.Entities;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(30)]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(60)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(40)]
+    public string Password { get; set; } = string.Empty;
+
+    //Relationship with Announcements many(Announcement) - to - one(User)
+    public ICollection<Announcement> Announcements { get; set; } = new List<Announcement>();
+}
