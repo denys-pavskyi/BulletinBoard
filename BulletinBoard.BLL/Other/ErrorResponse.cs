1835d5062080e8a001ad99588c9ac8a8bc53cedb
denys-pavskyi <den.pavski@gmail.com>
2025-05-08 22:16:49 +0300

    feat: Added custom Result<T> and ErrorResponse for future error handling

---
+++ BulletinBoard.BLL/Other/ErrorResponse.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json.Serialization;
+
+namespace BulletinBoard.BLL.Other;
+
+public class ErrorResponse
+{
+    public string? Message { get; set; }
+
+    [JsonIgnore]
+    public HttpStatusCode? HttpCode { get; set; } = HttpStatusCode.BadRequest;
+}
+
+public static class ErrorResponseExtensions
+{
+    public static ActionResult ToActionResult(this ErrorResponse errorResponse)
+    {
+        var statusCode = errorResponse.HttpCode ?? HttpStatusCode.BadRequest;
+        return statusCode switch
+        {
+            HttpStatusCode.OK => new OkObjectResult(errorResponse),
+            _ => new ObjectResult(errorResponse) { StatusCode = (int)statusCode }
+        };
+    }
+}
---
+++ BulletinBoard.BLL/Other/Result.cs
@@ -0,0 +1,23 @@
+﻿namespace BulletinBoard.BLL.Other;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+    public T? Value { get; }
+    public ErrorResponse? Error { get; }
+
+    private Result(T value)
+    {
+        IsSuccess = true;
+        Value = value;
+    }
+
+    private Result(ErrorResponse error)
+    {
+        IsSuccess = false;
+        Error = error;
+    }
+
+    public static Result<T> Success(T value) => new(value);
+    public static Result<T> Failure(ErrorResponse error) => new(error);
+}
