4d216a9414eebde42d23a84e49f5c0e30a4da393
denys-pavskyi <den.pavski@gmail.com>
2025-05-08 15:23:23 +0300

    feat: Added GetAllAnnouncements stored procedure through code-first approach

---
+++ BulletinBoard.DAL/Migrations/20250508121450_AddedGetAllAnnouncementsStoredProcedure.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulletinBoard.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedGetAllAnnouncementsStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            CREATE PROCEDURE GetAllAnnouncements
+            AS
+            BEGIN
+                SELECT [Id]
+                  ,[Title]
+                  ,[Description]
+                  ,[IsActive]
+                  ,[CreatedDate]
+                  ,[SubcategoryId]
+                  ,[UserId]
+              FROM [BulletinBoardDB].[dbo].[Announcements]
+            END
+        ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE GetAllAnnouncements");
+        }
+    }
+}
