271c9301e3317cacb35cc409217c07e25ce26152
denys-pavskyi <den.pavski@gmail.com>
2025-05-10 17:55:46 +0300

    refactor: added _AnnouncementList to separate it from main page, fixed checkboxed being unchecked after applyin filters, added isActive to filter

--- BulletinBoard.WebClient/Controllers/AnnouncementsController.cs
+++ BulletinBoard.WebClient/Controllers/AnnouncementsController.cs
@@ -15,20 +15,16 @@ namespace BulletinBoard.WebClient.Controllers
             _announcementService = announcementService;
         }
 
-
-        [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var allSubcategoryIds = CategoryData.Categories
-                .SelectMany(c => c.Subcategories)
-                .Select(s => s.Id)
-                .ToList();
+            var categories = CategoryData.Categories;
+            var allSubcategoryIds = categories.SelectMany(c => c.Subcategories.Select(s => s.Id)).ToList();
 
             var announcements = await _announcementService.GetFilteredAsync(allSubcategoryIds, true);
 
             var model = new AnnouncementIndexViewModel
             {
-                Categories = CategoryData.Categories,
+                Categories = categories,
                 Announcements = announcements,
                 SelectedSubcategoryIds = allSubcategoryIds,
                 IsActive = true
@@ -37,20 +33,45 @@ namespace BulletinBoard.WebClient.Controllers
             return View(model);
         }
 
-
         [HttpPost]
-        public async Task<IActionResult> Index(AnnouncementIndexViewModel model)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> FilterPartial([FromBody] FilterRequestDto request)
         {
-            var filteredAnnouncements = model.SelectedSubcategoryIds.Any()
-                ? await _announcementService.GetFilteredAsync(model.SelectedSubcategoryIds, model.IsActive)
-                : new List<AnnouncementViewModel>();
+            var announcements = await _announcementService.GetFilteredAsync(request.SubcategoryIds, request.IsActive);
 
+            foreach (var item in announcements)
+            {
+                var subcategory = CategoryData.Categories
+                    .SelectMany(c => c.Subcategories)
+                    .FirstOrDefault(sc => sc.Id == item.SubcategoryId);
 
-            model.Categories = CategoryData.Categories;
-            model.Announcements = filteredAnnouncements;
+                var category = CategoryData.Categories
+                    .FirstOrDefault(c => c.Subcategories.Any(sc => sc.Id == item.SubcategoryId));
 
-            return View(model);
+                if (subcategory != null && category != null)
+                {
+                    item.SubcategoryName = subcategory.Name;
+                    item.CategoryName = category.Name;
+                }
+            }
+
+            return PartialView("_AnnouncementList", announcements);
         }
+
+
+        //[HttpPost]
+        //public async Task<IActionResult> Index(AnnouncementIndexViewModel model)
+        //{
+        //    var filteredAnnouncements = model.SelectedSubcategoryIds.Any()
+        //        ? await _announcementService.GetFilteredAsync(model.SelectedSubcategoryIds, model.IsActive)
+        //        : new List<AnnouncementViewModel>();
+
+
+        //    model.Categories = CategoryData.Categories;
+        //    model.Announcements = filteredAnnouncements;
+
+        //    return View(model);
+        //}
 
         public IActionResult MyAnnouncements()
         {
